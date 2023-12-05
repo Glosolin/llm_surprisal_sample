070ef2a2162cde4ba7362c79f8799be6f722b4cd
Glosolin <zg745@nyu.edu>
2023-12-04 19:31:25 -0500

    optimize top-p implementation, add input argv[4]

--- main_sample.py
+++ main_sample.py
@@ -1,4 +1,10 @@
 """
+This code is designed for calculating surprisal using autoregressive language models (LMs)
+from the Huggingface transformers library. Its primary objective is to test the effects of 
+several sampling methods (top-k, top-p, temperature) on surprisal calculation. 
+
+The code supports various models from the GPT-2, GPT-Neo, and OPT families:
+
 GPT-2 family:
 "gpt2", "gpt2-medium", "gpt2-large", "gpt2-xl"
 
@@ -11,11 +17,10 @@ OPT family:
 "facebook/opt-6.7b", "facebook/opt-13b", "facebook/opt-30b", "facebook/opt-66b"
 """
 
-### need to implement temperature w/ top-k and temperature w/ top-k
-
 import os, sys, torch, transformers
 from transformers import AutoTokenizer, AutoModelForCausalLM, GPTNeoXTokenizerFast
 import pandas as pd
+import time
 
 
 def generate_stories(fn):
@@ -58,20 +63,24 @@ def main():
 
     results_list = []
     sampling_type = sys.argv[3]
-    if sampling_type == 'top-k':
-        sampling_values = [100] # [10, 50, 100, 1000, 2000, 10000, 25000, 50000]
-        # print('Top-k Sampling: ')
-    elif sampling_type == 'top-p':
-        sampling_values = [0.95, 0.9, 0.85, 0.8, 0.7]
-        # print('Top-p Sampling: ')
-    elif sampling_type == 'temperature':
-        sampling_values = [0.1, 0.3, 0.5, 1.0, 1.5, 10]
-    elif sampling_type == 'none':
-        sampling_values = [0]
-        # print('No Sampling: ')
-    else:
-        raise ValueError("Unsupported Sampling Method")
+    sampling_values = [float(sys.argv[4])]
+    sys.stderr.write(f"{sampling_type}: {sampling_values}\n")
     
+    # if sampling_type == 'top-k':
+    #     sampling_values = [[10, 50, 100, 1000, 2000, 10000, 25000, 50000]
+    #     # print('Top-k Sampling: ')
+    # elif sampling_type == 'top-p':
+    #     sampling_values = [0.95, 0.9, 0.85, 0.8, 0.7]
+    #     # print('Top-p Sampling: ')
+    # elif sampling_type == 'temperature':
+    #     sampling_values = [0.1, 0.3, 0.5, 1.0, 1.5, 10]
+    # elif sampling_type == 'none':
+    #     sampling_values = [0]
+    #     # print('No Sampling: ')
+    # else:
+    #     raise ValueError("Unsupported Sampling Method")
+    
+    start_time = time.time()
     for top_value in sampling_values:
         batches = []
         words = []
@@ -122,6 +131,9 @@ def main():
         curr_word_ix = 0
         curr_word_surp = []
         curr_toks = ""
+        
+        total_neg_log_likelihood = 0
+        total_tokens = 0
 
         print("word llm_surp")
         for batch in batches:
@@ -147,32 +159,44 @@ def main():
                 index = torch.arange(0, output_ids.shape[0])
                 actual_probs = new_probs[index, output_ids]
                 surp = -1 * torch.log2(actual_probs)
-
+                
             elif sampling_type == 'top-p':   # top-p
                 logits = model_output.logits
                 probs = softmax(logits)
                 sorted_probs, sorted_indices = torch.sort(probs, descending=True)
                 cum_probs = torch.cumsum(sorted_probs, dim=-1)
                 new_probs = torch.full_like(logits, fill_value=1e-10)
+                
+                cum_thresh = (cum_probs <= top_value).float()
+                cum_counts = cum_thresh.sum(dim=-1, keepdim=True)
+                cum_counts = torch.clamp(cum_counts, min=1)
                 for i in range(cum_probs.shape[0]):
-                    num_inc = sum(cum_probs[i]<=top_value).item()
-                    num_inc = max(1, num_inc)
-                    inc_probs = sorted_probs[i, :num_inc]
-                    inc_probs /=inc_probs.sum()
-                    new_probs[i, sorted_indices[i, :num_inc]] = inc_probs 
+                    count = int(cum_counts[i].item())
+                    if count == 1 and sorted_probs[i, 0] > top_value:
+                        # if only one token's probability is above the threshold, assign it a probability of 1 due to normalize
+                        new_probs[i, sorted_indices[i, 0]] = 1.0
+                    else:
+                        # normalize 
+                        top_probs = sorted_probs[i, :count]
+                        top_probs /= top_probs.sum(dim=-1, keepdim=True)
+                        new_probs[i, sorted_indices[i, :count]] = top_probs
 
                 index = torch.arange(0, output_ids.shape[0])
                 actual_probs = new_probs[index, output_ids]
+                actual_probs = torch.clamp(actual_probs, min=1e-10)
+                # sys.stderr.write(f"{torch.isnan(actual_probs)}\n")
                 surp = -1 * torch.log2(actual_probs)
 
             elif sampling_type == 'temperature':   # temperature
                 logits = model_output.logits
                 scaled_logits = logits / top_value
                 probs = softmax(scaled_logits)
-                
                 index = torch.arange(0, output_ids.shape[0])
                 actual_probs = probs[index, output_ids]
                 surp = -1 * torch.log2(actual_probs)
+            
+            total_neg_log_likelihood += surp.sum().item()
+            total_tokens += surp.size(0)
 
             for i in range(start_idx, len(toks)):
                 # necessary for diacritics in Dundee
@@ -191,6 +215,14 @@ def main():
                     curr_word_surp = []
                     curr_toks = ""
                     curr_word_ix += 1
+
+    # calculate model perplexity
+    average_neg_log_likelihood = total_neg_log_likelihood / total_tokens
+    perplexity = 2 ** average_neg_log_likelihood
+    sys.stderr.write(f"Perplexity: {perplexity}\n")
+    sys.stderr.write(f"{average_neg_log_likelihood}\n")
+    end_time = time.time()
+    sys.stderr.write(f"Time spent: {end_time - start_time}\n")
     # print out results
     df = pd.DataFrame(results_list)
     for col in df.columns:
